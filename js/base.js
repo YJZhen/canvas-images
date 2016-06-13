db5c32f05e8e8a8800f3a1ab7fc8eed21c31f4fc
僵尸粉YJ <yjzhen@live.com>
2016-06-13 21:51:21 +0800

    将HTML5中canvas标签的绘图以图片方式保存到本地

--- js/base.js
+++ js/base.js
@@ -6,6 +6,13 @@ define(function(){
                 return document.getElementById(select.substring(1));
             }
         },
+        /**
+         * 给元素绑定事件
+         * @param  {DOM Objct} _element 元素句柄
+         * @param  {String} _event   事件类型
+         * @param  {function} _fun     事件的回调函数
+         * @return 
+         */
         bindEvent:function(ele,eve,fun){
             if(window.attachEvent){
                 ele.attachEvent('on'+eve,function(_eve){
--- js/draw.js
+++ js/draw.js
@@ -6,63 +6,48 @@ define(['base','control-panel'],function(Base,controlPanel) {
     var canvasW=canvas.offsetWidth;
     var canvasH=canvas.offsetHeight;
 
-    /**
-     * 给元素绑定事件
-     * @param  {DOM Objct} _element 元素句柄
-     * @param  {String} _event   事件类型
-     * @param  {function} _fun     事件的回调函数
-     * @return 
-     */
-    function bindEvent(_element,_event,_fun){
-        if(window.attachEvent){
-            _element.attachEvent('on'+_event,_fun);
-        }
-        else{
-            _element.addEventListener(_event,_fun);
-        }
-    }
-
     function getScreenImage(_can){
         if(_can){
-            //canvas.toDataURL('image/png');
             screenImg.src=canvas.toDataURL('image/png');
-            //screenImg.src=canvas.getImageData();
         }
     }
     (function(){
         var y=0;
         var x=0;
-        var _drawStatus=false;
-        
-        bindEvent(canvas,'mousedown',function(_event){
-            _drawStatus=true;
-            canvasCtx.lineWidth=controlPanel.getPanSize();
-            canvasCtx.strokeStyle=controlPanel.color();
-            canvasCtx.beginPath();
+        var _drawStatus=false;//记录画笔是否可以绘画
+        //监听鼠标在canvas标签按下事件
+        Base.bindEvent(canvas,'mousedown',function(ele,_event){
+            _drawStatus=true;//当鼠标按下时，设未可以绘画
+            canvasCtx.lineWidth=controlPanel.getPanSize();//设置画笔的Size
+            canvasCtx.strokeStyle=controlPanel.color();//设置画笔的颜色
+            canvasCtx.beginPath();//开启一条新的路径
             canvasCtx.moveTo(_event.offsetX,_event.offsetY);
         });
-        bindEvent(canvas,'mousemove',function(_event){
-            if(!_drawStatus)return;
+        //监听鼠标在canvas标签的移动事件
+        Base.bindEvent(canvas,'mousemove',function(ele,_event){
+            if(!_drawStatus)return;//如果
             var _x=_event.screenX;
             var _y=_event.screenY;
             canvasCtx.lineTo(_event.offsetX,_event.offsetY);
             canvasCtx.stroke();
             getScreenImage(controlPanel.syncToImg());
         });
-        bindEvent(canvas,'mouseup',function(){
+        //监听鼠标在canvas标签的松开事件
+        Base.bindEvent(canvas,'mouseup',function(){
             _drawStatus=false;
             canvasCtx.closePath();
         });
     })();
-
-    bindEvent(Base.$('#load-img'),'click',function(){
+    //监听“加载图片”按钮的点击事件
+    Base.bindEvent(Base.$('#load-img'),'click',function(){
         var _img=new Image();
         _img.src="style/123.jpg";
         _img.onload=function(){
             canvasCtx.drawImage(_img,0,0);;
         }
     });
-    bindEvent(scrOpe,'click',function(){
+    //监听“生成图片”按钮的点击事件
+    Base.bindEvent(scrOpe,'click',function(){
         getScreenImage(true);
     });
     return null;    
